bbb089101697d2ce74e50e5f64303aedd4684cf8
bezzelek <bezzelek@gmail.com>
2020-06-24 23:25:27 +0300

    [add] sentry

--- src/root/settings.py
+++ src/root/settings.py
@@ -5,6 +5,7 @@ from os import getenv
 
 DEBUG = getenv('DEBUG', '1') in {'1', 'true', 'True'}
 DATABASE_URL = getenv('DB_URL', 'postgres://postgres@postgres:5432/covid19')
+SENTRY_URL = getenv('SN_URL')
 BROKER_URL = getenv('MB_URL', 'amqp://guest:guest@rabbit:5672/')
 CELERY_WORKERS = int(getenv('CELERY_WORKERS', '1'))
 
